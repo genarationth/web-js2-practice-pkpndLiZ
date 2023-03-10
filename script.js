c2d37583625a17e7bdef0f08bc722aa421103d69
pkpndLiZ <phanida.lip@gmail.com>
2023-03-10 22:05:13 +0700

    send part2 => challenge1: Create an Age Calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+// Part2: New contender
+
+// Challenge 1: Create an Age Calculator
+const myBirthYear = 1996;
+const futureYear = 2023;
+
+myPossibleAge = futureYear - myBirthYear;
+
+console.log(`If you were born in ${myBirthYear}, then in 2026 you’ll be ${myPossibleAge-1} or ${myPossibleAge} (depending on the month in 2026).`)
