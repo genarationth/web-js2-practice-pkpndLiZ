c0a25e8b1e3b17a785c79650920c9698a660f61f
pkpndLiZ <phanida.lip@gmail.com>
2023-03-10 22:27:42 +0700

    send part2 => challenge2: Create an information card

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // Part2: New contender
 
 // Challenge 1: Create an Age Calculator
-const myBirthYear = 1996;
-const futureYear = 2023;
+// const myBirthYear = 1996;
+// const futureYear = 2023;
 
-myPossibleAge = futureYear - myBirthYear;
+// myPossibleAge = futureYear - myBirthYear;
 
-console.log(`If you were born in ${myBirthYear}, then in 2026 you’ll be ${myPossibleAge-1} or ${myPossibleAge} (depending on the month in 2026).`)
+// console.log(`If you were born in ${myBirthYear}, then in 2026 you’ll be ${myPossibleAge-1} or ${myPossibleAge} (depending on the month in 2026).`)
+
+
+//Challenge 2: Create an information card
+
+let personInformation = {
+    name: "Pk",
+    location: "Bangkok",
+    hobby: "cooking"
+};
+
+console.log(`Hi, my name is ${personInformation.name}. I live in ${personInformation.location} and enjoy ${personInformation.hobby}.`)
